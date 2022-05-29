685003be6443ce05b5a766f3887437e8daa1c5c9
dwijanto11 <dwijanto10@gmail.com>
2022-05-29 14:47:03 +0700

    menambahkan jawaban

--- index.js
+++ index.js
@@ -1,16 +1,75 @@
 // TODO: Buatlah data dari kedua user di sini
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
-const firstUser = {};
-const secondUser = {};
+const firstUser = {
+    name : "Monica",
+    gender : "Female",
+    age : 17,
+    email : "monica@dingdong.com",
+    favoriteColor : ["Purple", "Red", "White", "Pink"],
+    isHavePet : "Yes",
+    education : [
+        {
+            name: "SD 01",
+            city: "Jakarta",
+            graduate: 2016
+        },
+        {
+            name: "SMP 02",
+            city: "Jakarta",
+            graduate: 2019
+        }, 
+        {
+            name: "SMA 03",
+            city: "Tangerang"
+        }
+    ],
+    favoriteRestaurant : ["KFC","MCD","PHD","Geprek","Bakso","Martabak","Lumpia"]
+};
+
+const secondUser = {
+    name : "Wendy",
+    gender : "Male",
+    age : 23,
+    email : "wendy@dingdong.com",
+    favoriteColor : ["Green", "Black", "Red"],
+    isHavePet : "No",
+    education : [
+        {
+            name: "SD 02",
+            city : "Jakarta",
+            graduate : 2010
+        }, 
+        {
+            name : "SMP 03",
+            city : "Bogor",
+            graduate : 2013
+        }, 
+        {
+            name : "SMA 01",
+            city: "Surabaya",
+            graduate: 2016
+        },
+        {
+            name : "Universitas Maju",
+            city : "Tangerang"
+        }
+    ],
+    favoriteRestaurant : ["Bakso","Dimsum","Martabak","Lumpia","Nasgor","Soto","Gulai","Sate"]
+
+};
 
 // TODO: Masukkan hasil akhir dari kedua user di sini
 const users = [];
+users.push(firstUser);
+users.push(secondUser);
+
 
 // ! JANGAN MODIFIKASI LINE DI BAWAH
 function main() {
     console.log(users.length || users.size);
     console.log(users);
+    console.log(users[0].favoriteColor)
 }
 
 main();
